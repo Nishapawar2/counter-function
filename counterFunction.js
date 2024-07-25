961225fcfc02e6f58030afe82c52df78bebc8fc5
Nisha Narayan Pawar <74763939+Nishapawar2@users.noreply.github.com>
2024-07-25 11:24:35 +0530

    Update counterFunction.js

--- counterFunction.js
+++ counterFunction.js
@@ -1,1 +1,14 @@
+export function createCounter(n) {
+	let  cntr = n;
+	return function () {
+		return cntr++;
+	};
+}
 
+const counter1 = createCounter(21);
+console.log(counter1()); 
+console.log(counter1()); 
+
+const counter2 = createCounter(31);
+console.log(counter2()); 
+console.log(counter2()); 
